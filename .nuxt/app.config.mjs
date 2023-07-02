4ed8a3381c4d88d6f0b65b67dd103f5460a4eb0c
shiwei93 <stayfocusjs@gmail.com>
2023-07-02 14:38:16 +0800

    feat: replace with nuxt

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/shiwei/Repos/Github/vue-fabric-demo/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
